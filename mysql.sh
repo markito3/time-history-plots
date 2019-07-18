da0429af3e26dba19538a81672105ff1ebb1602e
markito3 <marki@jlab.org>
2019-07-18 16:11:06 -0400

    squelch stdout

--- mysql.sh
+++ mysql.sh
@@ -2,5 +2,5 @@
 date=`date +%s`
 total=`mysql -B -N -hhallddb -umarki -phybrid information_schema -e 'select count(*) from processlist'`
 ccdb_farm=`mysql -B -N -hhallddb -uccdb_user information_schema -e 'select count(*) from processlist where user = "ccdb_user" and (host like "farm%" or host like "qcd%")'`
-echo $date,$total,$ccdb_farm
+#echo $date,$total,$ccdb_farm
 echo $date,$total,$ccdb_farm >> mysql.txt
