7896b5bbedb58a7fb9974c6eb4dc2138d0301f0a
Mark Ito <marki@jlab.org>
2019-08-26 10:54:23 -0400

    Create user-by-user data for farm jobs.

---
+++ plots/jobstat/user_jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+time=`date +%s`
+dataline=$time
+jsfile=/tmp/jobstat_for_user_jobs.txt
+rm -f $jsfile
+jobstat > $jsfile
+while read -r user
+do
+    echo $user
+    total=`grep " $user " $jsfile | wc -l`
+    running=`grep " $user " $jsfile | grep " R " | wc -l`
+    dataline=${dataline},${total},${running}
+    echo $dataline
+done < known_users.txt
+echo $dataline >> user_jobs.txt
