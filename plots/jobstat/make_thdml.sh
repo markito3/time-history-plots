6f8d8ee59cf3b654819b6e1281c2fb5784ad110a
markito3 <marki@jlab.org>
2019-08-26 13:46:16 -0400

    Create thdml file from list of known users.

---
+++ plots/jobstat/make_thdml.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cat <<EOF
+<time_history_data>
+<input_data file="/group/halld/time_history_data/user_jobs.txt"/>
+<columns>
+<column name="time" position="1"/>
+EOF
+n=2
+while read -r user
+do
+    echo user = $user
+    let m=n+1
+    cat <<EOF
+<column name="$user total" position="$n"/>
+<column name="$user running" position="$m"/>
+EOF
+    let n+=2
+done < known_users.txt
+cat <<EOF
+</columns>
+</time_history_data>
+EOF
