98838f87271fe196fdcda5674d0fd0896a6544bc
markito3 <marki@jlab.org>
2019-08-29 13:30:34 -0400

    Makes user job plots for fixed time duration and fixed cut on number of jobs total.

--- plots/jobstat/make_user_thpml.py
+++ plots/jobstat/make_user_thpml.py
@@ -1,9 +1,29 @@
 #!/bin/python
 import subprocess
+
+print("""\
+<time_history_plot>
+  <input_data_spec file="user_jobs.thdml"/>
+  <time_scale scale="hours"/>
+  <time_range type="latest" duration="4"/>
+  <time_zone offset="-04:00"/>""")
+
+
+with open('known_users.txt') as ku:
+    users = ku.readlines()
+users = [x.strip() for x in users]
+
 lastLineOutput = subprocess.Popen(['tail', '-1', '/home/marki/google_drive/scratch/user_jobs.txt'],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 stdout,stderr = lastLineOutput.communicate()
-print(stdout)
-print(stderr)
 lastLine = stdout.rstrip()
 counts = lastLine.split(',')
-print(counts)
+i = 1
+while i < len(counts):
+    totalJobs = int(counts[i])
+    if totalJobs > 10:
+        userIndex = (i - 1)/2 
+        print('  <plot x="time" y="' + users[userIndex] + ' running"/>')
+    i += 2
+print("""\
+</time_history_plot>
+""")
