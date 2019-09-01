0ee91923a8d7393d68c50e47aaac31c202eded7d
Mark Ito <marki@jlab.org>
2019-08-31 21:46:07 -0400

    Lose debug output.

--- plots/jobstat/check_and_add.sh
+++ plots/jobstat/check_and_add.sh
@@ -2,16 +2,13 @@
 newuser=$1
 if [ "$newuser" == "" ]
 then
-    echo blank user, exiting
     exit 1
 fi
 known_users=/group/halld/time_history_data/known_users.txt
 grep -q $newuser $known_users
 status_grep=$?
-if [ $status_grep == 0 ]
+if [ $status_grep != 0 ]
 then
-    echo $newuser found
-else
-    echo $newuser not found
+    echo $newuser not found, adding to list
     echo $newuser >> $known_users
 fi
