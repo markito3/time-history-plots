a12727cd4889d78bb790ee9982357f51fae8071a
markito3 <marki@jlab.org>
2019-09-09 14:22:23 -0400

    Add some set-up scripts.

---
+++ scripts/setup.csh
@@ -0,0 +1,4 @@
+if (! $?THP_HOME) then
+    setenv THP_HOME /group/halld/Software/time-history-plots
+endif
+setenv PATH $THP_HOME/plots/jobstat:$THP_HOME/scripts:$PATH
---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+if [ -z "$THP_HOME" ]
+then
+    export THP_HOME=/group/halld/Software/time-history-plots
+fi
+export PATH=$THP_HOME/plots/jobstat:$THP_HOME/scripts:$PATH
