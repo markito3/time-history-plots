de886edfbdb4a70ed9d97ec662e16a4f034b66e1
markito3 <marki@jlab.org>
2019-09-13 11:45:34 -0400

    Add hallddb-a and hallddb-b to list of nodes to track.

--- plots/mysql/mysql.sh
+++ plots/mysql/mysql.sh
@@ -4,4 +4,6 @@ hallddb=`mysql -B -N -hhallddb -umarki -phybrid information_schema -e 'select co
 hallddb_ext=`mysql -B -N -hhallddb-ext -umarki -phybrid information_schema -e 'select count(*) from processlist'`
 halldweb=`mysql -B -N -hhalldweb -umarki -phybrid information_schema -e 'select count(*) from processlist'`
 ccdb_farm=`mysql -B -N -hhallddb -uccdb_user information_schema -e 'select count(*) from processlist where user = "ccdb_user" and (host like "farm%" or host like "qcd%")'`
-echo $date,$hallddb,$ccdb_farm,$hallddb_ext,$halldweb >> /group/halld/time_history_data/mysql.txt
+hallddb_a=`mysql -B -N -hhallddb-a -umarki -phybrid information_schema -e 'select count(*) from processlist'`
+hallddb_b=`mysql -B -N -hhallddb-b -umarki -phybrid information_schema -e 'select count(*) from processlist'`
+echo $date,$hallddb,$ccdb_farm,$hallddb_ext,$halldweb,$hallddb_a,$hallddb_b >> /group/halld/time_history_data/mysql.txt
