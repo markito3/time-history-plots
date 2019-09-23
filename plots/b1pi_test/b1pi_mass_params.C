9af7b711bfbbf80813b95742f82be7c2345db62f
markito3 <marki@jlab.org>
2019-09-23 16:49:19 -0400

    outputs parameters from hardwired h1

--- plots/b1pi_test/b1pi_mass_params.C
+++ plots/b1pi_test/b1pi_mass_params.C
@@ -1,5 +1,10 @@
 {
-  TFile *F1 = new TFile("hd_root.root");
+  TFile *F1 = new TFile("/scratch/gluex/b1pi/2019-09-19/Linux_RHEL7-x86_64-gcc4.8.5/11366/hd_root.root");
   TH1I *x_mass_hist = (TH1I*)F1->Get("b1pi/Custom_HistMass_X_2000/InvariantMass");
   x_mass_hist->Draw();
+  Double_t entries = x_mass_hist->GetEntries();
+  Double_t mean = x_mass_hist->GetMean(1);
+  Double_t sigma = x_mass_hist->GetMean(11);
+  Double_t rms = x_mass_hist->GetStdDev(1);
+  cout << "entries=" << entries << "," << "mean=" << mean << "," << "sigma=" << sigma << "," << "rms=" << rms << endl;
 }
