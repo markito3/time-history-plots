ebfa54a3db341c635fc0a703b64086944aa55218
markito3 <marki@jlab.org>
2019-10-08 16:25:50 -0400

    use production location of time-history-plots

--- plots/b1pi_test/b1pi_history_data.sh
+++ plots/b1pi_test/b1pi_history_data.sh
@@ -6,6 +6,5 @@ if ! utime=`date -d $date_in +%s`
     exit 1
 fi
 export THP_HOME=/group/halld/Software/time-history-plots
-export THP_HOME=/home/marki/time-history-plots
 root_output=`root -b -q '$THP_HOME/plots/b1pi_test/b1pi_mass_params.C("'$date_in'")' | grep hist_params: | awk -F"[,=]" 'BEGIN{ORS=""}{print ","$3","$5","$7","$9}'`
 echo ${utime}${root_output}
