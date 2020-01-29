e670b5f0a748a992949f10303ffb904d8128f4ad
markito3 <marki@jlab.org>
2020-01-29 14:48:11 -0500

    Make entries for connections to halddb-c and halddb-d.

--- plots/mysql/mysql.sh
+++ plots/mysql/mysql.sh
@@ -6,4 +6,6 @@ halldweb=`mysql -B -N -hhalldweb -umarki -phybrid information_schema -e 'select 
 ccdb_farm=`mysql -B -N -hhallddb -uccdb_user information_schema -e 'select count(*) from processlist where user = "ccdb_user" and (host like "farm%" or host like "qcd%")'`
 hallddb_a=`mysql -B -N -hhallddb-a -umarki -phybrid information_schema -e 'select count(*) from processlist'`
 hallddb_b=`mysql -B -N -hhallddb-b -umarki -phybrid information_schema -e 'select count(*) from processlist'`
-echo $date,$hallddb,$ccdb_farm,$hallddb_ext,$halldweb,$hallddb_a,$hallddb_b >> /group/halld/time_history_data/mysql.txt
+hallddb_c=`mysql -B -N -hhallddb-c -umarki -phybrid information_schema -e 'select count(*) from processlist'`
+hallddb_d=`mysql -B -N -hhallddb-d -umarki -phybrid information_schema -e 'select count(*) from processlist'`
+echo $date,$hallddb,$ccdb_farm,$hallddb_ext,$halldweb,$hallddb_a,$hallddb_b,$hallddb_c,$hallddb_d # >> /group/halld/time_history_data/mysql.txt
