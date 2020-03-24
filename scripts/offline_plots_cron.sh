e5e27545ad1372733ff41b443a47b00934d7202d
markito3 <marki@jlab.org>
2020-03-24 13:15:51 -0400

    export the THP variable

--- scripts/offline_plots_cron.sh
+++ scripts/offline_plots_cron.sh
@@ -2,6 +2,7 @@
 thp_home=/group/halld/Software/time-history-plots
 thp_data=/group/halld/time_history_data
 thp_web=/group/halld/www/halldweb/html/disk_management
+export THP=$thp_home
 export PATH=$thp_home/plots/jobstat:$thp_home/scripts:$PATH
 cd $thp_data
 make_user_thpml.py > user_jobs.thpml.template.tmp
