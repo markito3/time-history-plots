ed7bce616143d09e6295e84f9b4e36ea4101b3d4
markito3 <marki@jlab.org>
2020-03-24 11:05:39 -0400

    make a script to replace complicated crontab line

---
+++ scripts/offline_plots_cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+thp_home=/group/halld/Software/time-history-plots
+thp_data=/group/halld/time_history_data
+thp_web=/group/halld/www/halldweb/html/disk_management
+export PATH=$thp_home/plots/jobstat:$thp_home/scripts:$PATH
+cd $thp_data
+make_user_thpml.py > user_jobs.thpml.template.tmp
+mv user_jobs.thpml.template.tmp user_jobs.thpml.template
+cd $thp_web
+make_status_page.sh
