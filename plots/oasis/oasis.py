5e4b4d6470578ff19da2310a6e2a388bab9d65aa
GlueX-Hall-D (Mark Ito) <gluex@sandd1.jlab.org>
2020-11-30 10:12:30 -0500

    add random noise to oasis size to avoid y-range = 0

--- plots/oasis/oasis.py
+++ plots/oasis/oasis.py
@@ -8,7 +8,7 @@ line = du_proc.stdout.readline()
 a = line.split()
 size_kB = float(a[0])
 # add a random number to the size to avoid plots with no y-range
-size_GB = size_kB/1024.0/1024.0 + random.random()
+size_GB = size_kB/1024.0/1024.0 + random.uniform(-0.1, 0.1)
 #print(now, size_GB)
 file_out = open("/group/halld/time_history_data/oasis.txt", "a+")
 file_out.write("%s,%s\n" % (now, size_GB))
