84bc595e66bdd117c93900bfe4a67cdb37196c3a
Mark Ito <marki@jlab.org>
2021-04-24 16:27:07 -0400

    Add /work/halld3 to list of partitions for which data are collected.

--- plots/df/df.py
+++ plots/df/df.py
@@ -2,7 +2,7 @@
 import subprocess
 import time
 now = time.time()
-df_proc = subprocess.Popen(['df', '/group/halld/', '/work/halld/'], stdout=subprocess.PIPE)
+df_proc = subprocess.Popen(['df', '/group/halld/', '/work/halld/', '/work/halld3/'], stdout=subprocess.PIPE)
 line = df_proc.stdout.readline()
 line = df_proc.stdout.readline()
 a = line.split()
@@ -14,7 +14,12 @@ a = line.split()
 wsize = a[1]
 wused = a[2]
 wavail = a[3]
+line = df_proc.stdout.readline()
+a = line.split()
+w3size = a[1]
+w3used = a[2]
+w3avail = a[3]
 #print(now, gsize, gused, gavail, wsize, wused, wavail)
 file_out = open("/group/halld/time_history_data/df.txt", "a+")
-file_out.write("%s,%s,%s,%s,%s,%s,%s\n" % (now, gsize, gused, gavail, wsize, wused, wavail))
+file_out.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (now, gsize, gused, gavail, wsize, wused, wavail, w3size, w3used, w3avail))
 file_out.close()
